9a7a9f1498219309f850b528857085ce17ae12d5
MARCOS AURELIO DO NASCIMENTO <marcos.anascimento12@senacsp.edu.br>
2025-03-24 21:30:33 -0300

    Validação

--- parouimpar.js
+++ parouimpar.js
@@ -4,29 +4,33 @@ const mostrarResultadoJs = document.getElementById("mostrarResultado");
 
 function MostrarResultado() {
 
-    //Declarar e inializar a variavel número//
-    var numeroInteiro = 0;
+    if (numeroJs.value != "") {
+        //Declarar e inializar a variavel número//
+        var numeroInteiro = 0;
 
-    //Declarar e inicializar a variavel mensagem//
-    var mensagemClc = "";
+        //Declarar e inicializar a variavel mensagem//
+        var mensagemClc = "";
 
-    //coletar o valor digitado no numero
-    numeroInteiro = numeroJs.value;
+        //coletar o valor digitado no numero
+        numeroInteiro = numeroJs.value;
 
-    //Declarar e inicializar a variavel rest//
-    var resto = 0;
+        //Declarar e inicializar a variavel rest//
+        var resto = 0;
 
-    //Dividir o numero por 2 e obeter o resto//
-    resto = numeroInteiro % 2;
+        //Dividir o numero por 2 e obeter o resto//
+        resto = numeroInteiro % 2;
 
-    //gerar a mensagem com base no conteudo do rsto//
-    if (resto == 0) {
-        mensagemClc = "O numero informado é Par"
+        //gerar a mensagem com base no conteudo do rsto//
+        if (resto == 0) {
+            mensagemClc = "O numero informado é Par"
+        }
+        else {
+            mensagemClc = "Onumero informado é Impar"
+        }
+        mensagemJs.innerText = mensagemClc;
+    }else{
+        alert("Por favor informe um numero inteiro para prosseguir");
     }
-    else {
-        mensagemClc = "Onumero informado é Impar"
-    }
-    mensagemJs.innerText = mensagemClc;
 
 }
 mostrarResultadoJs.addEventListener("click", MostrarResultado)
